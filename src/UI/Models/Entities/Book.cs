ae400f568062c6ab6e1b87ef1313d544174aaf56
Emad Gerami <emad.grnd@gmail.com>
2025-10-28 14:33:22 +0330

    Create initial entities

--- src/UI/Models/Entities/Book.cs
+++ src/UI/Models/Entities/Book.cs
@@ -1,19 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace UI.Models
+namespace UI.Models.Entities
 {
     public class Book
     {
         public int Id { get; set; }
+
         [Display(Name = "عنوان کتاب")]
         public string Title { get; set; }
+
+        [Display(Name = "نام نویسنده")]
+        public string AuthorName { get; set; }
+
         [Display(Name = "قیمت")]
         public decimal Price { get; set; }
+
+        [Display(Name = "تعداد صفحات")]
+        public int PagesCount { get; set; }
+
         [Display(Name = "آدرس تصویر")]
         public string ImageUrl { get; set; }
+
         [Display(Name = "تاریخ ثبت")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+
         public int CategoryId { get; set; }
         public Category Category { get; set; }
     }
--- src/UI/Models/Entities/Category.cs
+++ src/UI/Models/Entities/Category.cs
@@ -1,14 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace UI.Models
+namespace UI.Models.Entities
 {
     public class Category
     {
         public int Id { get; set; }
+
         [Display(Name = "عنوان دسته‌بندی")]
         public string Name { get; set; }
+
+        [Display(Name = "توصیف مختصر")]
+        public string BriefDescription { get; set; }
+
         [Display(Name = "آدرس تصویر")]
         public string ImageUrl { get; set; }
+
         public List<Book> Books { get; set; }
     }
 }
