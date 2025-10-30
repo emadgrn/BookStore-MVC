3187738f062701dc627ea845aad3d2160fffd359
Emad Gerami <emad.grnd@gmail.com>
2025-10-31 01:00:25 +0330

    add view for book controller

---
+++ src/UI/Models/DTOs/CreateBookDto.cs
@@ -0,0 +1,12 @@
+﻿namespace UI.Models.DTOs
+{
+    public class CreateBookDto
+    {
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public decimal Price { get; set; }
+        public int PagesCount { get; set; }
+        public int CategoryId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -14,7 +14,7 @@ namespace UI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
