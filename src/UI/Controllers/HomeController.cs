3a780ba9adb9335c8f9678ed497629d2127eaf67
Emad Gerami <emad.grnd@gmail.com>
2025-10-30 09:13:03 +0330

    modify HomeController

--- src/UI/Controllers/HomeController.cs
+++ src/UI/Controllers/HomeController.cs
@@ -1,21 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using UI.Contracts.Services;
 using UI.Models;
+using UI.Models.ViewModels;
 
 namespace UI.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IBookService _bookService;
+    private readonly ICategoryService _categoryService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IBookService bookService,ICategoryService categoryService)
     {
         _logger = logger;
+        _bookService = bookService;
+        _categoryService = categoryService;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var model = new HomeViewModel
+        {
+            LatestBooks = _bookService.GetLimitedLatestBooks(),
+            Categories = _categoryService.GetAll()
+        };
+
+        return View(model);
     }
 
     public IActionResult Privacy()
