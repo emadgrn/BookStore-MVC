6e4a3c04ba96d3055729d777e09e70f5e7bc52ea
Emad Gerami <emad.grnd@gmail.com>
2025-10-31 01:00:25 +0330

    add view for book controller

---
+++ src/UI/Models/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using UI.Models.Entities;
+
+namespace UI.Models.ViewModels
+{
+    public class CreateBookViewModel
+    {
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public decimal Price { get; set; }
+        public int PagesCount { get; set; }
+        public int CategoryId { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
