87a3e084588e99a636a4d53b73ff38d429bb25c9
Emad Gerami <emad.grnd@gmail.com>
2025-10-31 02:34:04 +0330

    modify books service using file service

--- src/UI/Services/BookService.cs
+++ src/UI/Services/BookService.cs
@@ -1,21 +1,36 @@
 ﻿using UI.Contracts.Repositories;
 using UI.Contracts.Services;
+using UI.Data.Repositories;
 using UI.Models.DTOs;
+using UI.Models.Entities;
 using UI.Models.ViewModels;
 
 namespace UI.Services
 {
     public class BookService(IBookRepository bookRepo): IBookService
     {
+
         public List<GetBookDto> GetLimitedLatestBooks()
         {
             const int topItems = 5;
             return bookRepo.GetLatestBooks(topItems);
         }
 
-        public void CreateNewBook(CreateBookDto model)
+        public void CreateNewBook(CreateBookViewModel model)
         {
-            bookRepo.Create(model);
+            var imgAddress = FileService.Upload(model.ImageFile);
+
+            var modelDto = new CreateBookDto()
+            {
+                Title = model.Title,
+                AuthorName = model.AuthorName,
+                PagesCount = model.PagesCount,
+                Price = model.Price,
+                CategoryId = model.CategoryId,
+                ImageUrl = imgAddress
+            };
+            bookRepo.Create(modelDto);
         }
+
     }
 }
---
+++ src/UI/Services/FileService.cs
@@ -0,0 +1,26 @@
+﻿namespace UI.Services
+{
+    public static class FileService
+    {
+        public static string Upload(IFormFile file)
+        {
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images","books");
+
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return $"{Path.Combine("images","books", uniqueFileName)}";
+        }
+    }
+}
