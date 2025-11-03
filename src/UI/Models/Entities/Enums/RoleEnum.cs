f0628497debabf27650250c9090104704b3f4bd0
Emad Gerami <emad.grnd@gmail.com>
2025-11-03 10:24:02 +0330

    add user entity

---
+++ src/UI/Models/Entities/Enums/RoleEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace UI.Models.Entities.Enums;
+
+public enum RoleEnum
+{
+    Admin = 1,
+    Customer = 2
+}
---
+++ src/UI/Models/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using UI.Models.Entities.Enums;
+
+namespace UI.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public RoleEnum Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
