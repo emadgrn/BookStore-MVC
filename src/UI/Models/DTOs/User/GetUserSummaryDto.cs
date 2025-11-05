c09c19e2013248d04c06f0654d225ccac8305c95
Emad Gerami <emad.grnd@gmail.com>
2025-11-06 01:33:39 +0330

    add get summary user dto

---
+++ src/UI/Models/DTOs/User/GetUserSummaryDto.cs
@@ -0,0 +1,12 @@
+﻿using UI.Models.Entities.Enums;
+
+namespace UI.Models.DTOs.User
+{
+    public class GetUserSummaryDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public RoleEnum Role { get; set; }
+    }
+}
