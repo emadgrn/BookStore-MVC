83641baaa62ed948dc08d4be8901259878eba0d3
Emad Gerami <emad.grnd@gmail.com>
2025-11-05 10:10:37 +0330

    add login view model instead of dto

--- src/UI/Models/DTOs/User/LoginDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UI.Models.DTOs.User;
-
-public class LoginDto
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
---
+++ src/UI/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace UI.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Mobile { get; set; }
+        public string Password { get; set; }
+    }
+}
